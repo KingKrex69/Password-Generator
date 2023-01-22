b85cecb42e4c6b220d35b4060ebbfdc3d392862c
KingKrex <116029736+KingKrex69@users.noreply.github.com>
2023-01-22 23:48:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,18 @@
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
-// #include <unistd.h>
 
+int main(void) {
+    char *res = malloc(17), 
+               *listing = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789";
 
-int main() {
+    srand((unsigned int)time(NULL));
 
-    int lenght = 16;
-    char *password = malloc(lenght + 1);
+    for(int i = 0; i < 16; ++i)
+        res[i] = listing[rand() % 73];
 
-    char *upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *lower = "abcdefghijklmnopqrstuvwxyz";
-    char *num = "0123456789";
-    char *symbol = "<>,.?/;:'[]{}()!@#$%^&*";
-    
-    int upper_lenght = strlen(upper);
-    int lower_lenght = strlen(lower);
-    int num_lenght = strlen(num);
-    int symbol_lenght = strlen(symbol);
+    res[16] = '\0';
 
-    srand(trime(NULL) * getpid());
-
-    for (int i = 0; i < lenght; i++){
-        int c_type = rand() % 4;
-
-        if (c_type == 0)
-            password[i] = num[rand() % num_lenght];
-
-        else if (c_type == 1)
-            password[i] = lower[rand() % lower_lenght];
-
-        else if (c_type == 2)
-            password[i] = upper[rand() % upper_lenght];
-
-        else
-            password[i] = symbol[rand() % symbol_lenght];
-
-        password[lenght] = '\0';
-
-        printf("%s\n", password);
-        free(password);
-
-
-        return 0;
-
-    }
+    puts(res);
+    free(res);
 }
